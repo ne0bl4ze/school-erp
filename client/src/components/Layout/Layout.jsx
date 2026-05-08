8d84b6050719c18658cd352512164df6d8a2d4e8
Cherukupally Ajay Simha Reddy <ajayreddy3321@gmail.com>
2026-05-08 09:44:55 +0530

    remove subnav bar

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import SubNav from './SubNav';
 import styles from './Layout.module.css';
 
 export default function Layout() {
   return (
     <div className={styles.app}>
       <Header />
-      <SubNav />
       <div className={styles.body}>
         <Sidebar />
         <main className={styles.main}>
